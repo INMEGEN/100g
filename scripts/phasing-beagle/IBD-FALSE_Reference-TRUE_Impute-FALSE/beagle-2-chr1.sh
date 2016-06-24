9bb4b16a75fcec979bf39aa054847647987de4af
FRPV <frpvillatoro@gmail.com>
2016-06-24 17:40:07 -0500

    Script for beagle, no-IBD reference and no-Impute

---
+++ scripts/phasing-beagle/IBD-FALSE_Reference-TRUE_Impute-FALSE/beagle-2-chr1.sh
@@ -0,0 +1,10 @@
+#BSUB-e BGL-2-1.err.%J
+#BSUB-o BGL-2-1.out.%J
+#BSUB-J BGL-2-1
+#BSUB-n 16
+
+cd /scratch/inmegen/100g/phasing
+export PATH=$PATH:/scratch/inmegen/100g/references/JAVA/jdk1.8.0_74/bin/
+
+java8 -Xmx12g -jar /home/inmegen/r.garcia/src/beagle.03May16.862.jar gt=/scratch/inmegen/100g/wg_GATK/test3/allsamples_final_recaled_snp-indel.vcf chrom=1 ref=/scratch/inmegen/100g/references/1000g-phase_3-allele-frequency/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5_extra_anno.20130502.genotypes.vcf.gz out=chr1_2 impute=false ibd=false
+
