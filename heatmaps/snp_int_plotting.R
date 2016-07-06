bbacca012b8dc015bd04710a3eef669b879c942a
hachepunto <hachepunto@gmail.com>
2016-07-05 22:58:07 -0500

    correcting data with square matrix

--- heatmaps/snp_int_plotting.R
+++ heatmaps/snp_int_plotting.R
@@ -5,84 +5,65 @@ library(gtools)
 library(ggdendro)
 library(RColorBrewer)
 library(stats)
+library(Matrix)
 
 source("heatmap.2a.R")
 source("A2Rplot.R")
 
-library("Matrix")
-source("jaccard.R")
-source("sif_to_matrix_function.R")
 
 
-jindex_sif <- read.table("jacaressif.txt", sep = "\t", header = TRUE, )
+# jindex_sif <- read.table("jacaressif.txt", sep = "\t", header = TRUE, )
 
-
-boxplot(jindex_sif[,3])
-
-lg <- jindex_sif
-lg[,3] <- jindex_sif[,3] * 100000
-
-summary(lg)
-
-matrix <- sif_to_matrix(jindex_sif, row_column=1, column_column=2, interaction_column=3)
-matrixS <- matrix[sort(rownames(matrix)),sort(colnames(matrix))]
-length(rownames(matrixS))
-
-
-
-
-sample_names <- read.table("sample_names.tsv", header = TRUE)
+jinM <- read.table("jinx_matrix.tsv", sep = "\t", header = FALSE)
+jinM <- as.matrix(jinM)
+dim(jinM)
+sample_names <- read.table("sample_names.tsv", header = FALSE)
 sn <- sample_names[,2]
 names(sn)<-sample_names[,1]
-sn <- sn[sort(names(sn))]
-sn <- sn[-c(47,91)] # bad sample
-
 names(sn)
-rownames(matrixS) <- sn
-colnames(matrixS) <- sn
+rownames(jinM) <- sn
+colnames(jinM) <- sn
+
+# dist metodos = "euclidean", "maximum", "manhattan", "canberra", "binary" or "minkowski"
+# Métodos de agrupamiento = "ward.D", "ward.D2", "single", "complete", "average" (= UPGMA), "mcquitty" (= WPGMA), "median" (= WPGMC) or "centroid" (= UPGMC).
 
 
-MA <- matrixS
-rownames(MA) <- sn
-colnames(MA) <- sn
+# classic dendrogram
+Md <- dist(jinM, method= "manhattan",  upper = TRUE, p=2)
+Mc <- hclust(Md, method = "ward.D")
+pdf("dendrograma.pdf",width=10,height=10)
+ggdendrogram(Mc, rotate = TRUE, size = 4, theme_dendro = FALSE, color = "black")
+dev.off()
 
-write.table(M, file = "M.tsv", quote = FALSE, sep = "\t", )
+# colored dendrogram
+pdf("color_dendrograma.pdf",width=20,height=12)
+hc = hclust(dist(jinM, method= "manhattan",),method = "ward.D")
+op = par(bg = "gray15")
+A2Rplot(hc, 
+  k = 8, 
+  boxes = FALSE, 
+  col.up = "gray50", 
+  #col.down = c("#d73027", "#fc8d59", "#fee090", "#e0f3f8", "#91bfdb", "#4575b4")
+  col.down = c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf')
+  )
+dev.off()
 
-# snp_int <- read.table("snp_intersections_matrix.txt", sep="\t", header = TRUE, row.names=1,colClasses = "character")
-# snp_int <- data.matrix(snp_int)
-# labels <- row.names(snp_int)
-
-M <- matrixS
-labels <- row.names(matrixS)
+M <- jinM
 labelsA <- as.character(sn)
 labelsB <- as.character(sn)
 labelsA[1]<- c(" ")
 labelsB[93]<- c(" ")
 
-# summary(jindex_sif[,3])
-#   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-#  0.4754  0.5419  0.5509  0.5476  0.5566  0.7411 
 
-quantile(seq(0.4754,0.5419, length=250), probs = seq(0, 1, 0.5))
-quantile(seq(0.5419, 0.5566, length = 3500), probs = seq(0, 1, 0.15))
-quantile(seq(0.5566, 0.7411, length=250), probs = seq(0, 1, 0.5))
-
-0.541900, 0.544105, 0.546310, 0.548515, 0.550720, 0.552925, 0.555130,
-
-
-#quantile(jindex_sif[,3], probs = seq(0, 1, 0.1249))
-# 0.4754440, 0.5326322, 0.5419321, 0.5474486, 0.5508673, 0.5536036, 0.5566329, 0.5604464, 0.7034893
-
-
-colours <- colorRampPalette(c('#f7f4f9','#e7e1ef','#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#91003f'))(n = length(table(jindex_sif[,3]))-1)
-col_breaks = c(seq(0.47540, 0.54190, length=200),
-    seq(0.541900, 0.544105, length=200),
-    seq(0.544105, 0.546310, length=200),
-    seq(0.546310, 0.548515, length=200),
-    seq(0.548515, 0.550720, length=200),
-    seq(0.550720, 0.552925, length=200),
-    seq(0.552925, 0.555130, length=200),
-    seq(0.5566, 0.74110, length=200))
+colours <- colorRampPalette(c('#f7fcfd','#e0ecf4','#bfd3e6','#9ebcda','#8c96c6','#8c6bb1','#88419d','#6e016b'))(n = 800-1)
+col_breaks = c(seq(0.47540, 0.54190, length=100),
+    seq(0.541901, 0.544105, length=100),
+    seq(0.544106, 0.546310, length=100),
+    seq(0.546311, 0.548515, length=100),
+    seq(0.548516, 0.550720, length=100),
+    seq(0.550721, 0.552925, length=100),
+    seq(0.552926, 0.555130, length=100),
+    seq(0.555131, 0.74110, length=100))
 
 
 
@@ -92,16 +73,17 @@ colfunc <- colorRampPalette(c("blue", "red"))
 M[lower.tri(M,diag = TRUE)] <- NA
 M <- t(M)
 
-pdf("triangulo1.pdf",width=200,height=200)
-heatmap.2a(M[-1,-93],
+pdf("triangulo.pdf",width=200,height=200)
+heatmap.2a(M,
   #cellnote = M,  # same data set for cell labels
   #notecol="black",      # change font color of cell labels to black
   density.info="none",  # turns off density plot inside color legend
   trace="none",         # turns off trace lines inside the heat map
   margins =c(26,25),     # widens margins around plot
-  col=colfunc(length(table(jindex_sif[,3]))),       # use on color palette defined earlier 
-  #breaks=col_breaks,    # enable color transition at specified limits
-  dendrogram="row",     # only draw a row dendrogram
+  #col=colfunc(length(table(jindex_sif[,3]))),       # use on color palette defined earlier 
+  col=colours,       # use on color palette defined earlier 
+  breaks=col_breaks,    # enable color transition at specified limits
+  dendrogram="none",     # only draw a row dendrogram
   Colv="NA",            # turn off column clustering
   cexCol =4,    # Tamaño de letra de las columnas
   cexRow =4,    # Tamaño de letra de los renglones
@@ -115,19 +97,20 @@ heatmap.2a(M[-1,-93],
   )
 dev.off()
 
-# Trianglular heatmap ordered
-Mx <- matrixS[Mc$order,Mc$order]
+# Ordered trianglular heatmap 
+Mx <- jinM[Mc$order,Mc$order]
 Mx[lower.tri(Mx,diag = TRUE)] <- NA
 Mx <- t(Mx)
-pdf("ordered_triangulo1.pdf",width=200,height=200)
+pdf("ordered_triangulo.pdf",width=200,height=200)
 heatmap.2a(Mx,
   #cellnote = M,  # same data set for cell labels
   #notecol="black",      # change font color of cell labels to black
   density.info="density",  # turns off density plot inside color legend
   trace="none",         # turns off trace lines inside the heat map
   margins =c(26,25),     # widens margins around plot
-  col=colfunc(length(table(jindex_sif[,3]))),       # use on color palette defined earlier 
-  #breaks=col_breaks,    # enable color transition at specified limits
+  #col=colfunc(length(table(jindex_sif[,3]))),       # use on color palette defined earlier 
+  col=colours,       # use on color palette defined earlier 
+  breaks=col_breaks,    # enable color transition at specified limits
   dendrogram="none",     # only draw a row dendrogram
   Colv="NA",            # turn off column clustering
   cexCol =4,    # Tamaño de letra de las columnas
@@ -143,45 +126,29 @@ dev.off()
 
 
 # classic heatmap
-pdf("heatmap1.pdf",width=200,height=200)
-heatmap.2(matrixS,
+pdf("heatmap.pdf",width=200,height=200)
+heatmap.2(as.matrix(jinM),
 	density.info="density",  # turns off density plot inside color legend
-	trace="none",         # turns off trace lines inside the heat map
-	col=colfunc(length(table(jindex_sif[,3]))),       # use on color palette defined earlier 
-#	breaks=col_breaks,    # enable color transition at specified limits
+      key=TRUE,
+      trace="none",         # turns off trace lines inside the heat map
+	#col=colfunc(length(table(jindex_sif[,3]))),       # use on color palette defined earlier 
+      col=colours,       # use on color palette defined earlier 	
+      breaks=col_breaks,    # enable color transition at specified limits
 	cexCol =6,    # Tamaño de letra de las columnas
 	cexRow =6,    # Tamaño de letra de los renglones
 	margins =c(26,25),     # widens margins around plot
 	)
 dev.off()
 
-# classic dendrogram
-Md <- dist(matrixS, method= "minkowski",  upper = TRUE, p=2)
-Mc <- hclust(Md, method = "complete")
-pdf("dendrograma1.pdf",width=10,height=10)
-ggdendrogram(Mc, rotate = TRUE, size = 4, theme_dendro = FALSE, color = "black")
-dev.off()
-
-# colored dendrogram
-pdf("color_dendrograma1.pdf",width=20,height=12)
-hc = hclust(dist(matrixS))
-op = par(bg = "gray15")
-A2Rplot(Mc, 
-	k = 6, 
-	boxes = FALSE, 
-	col.up = "gray50", 
-	col.down = c("#d73027", "#fc8d59", "#fee090", "#e0f3f8", "#91bfdb", "#4575b4")
-	)
-dev.off()
 
 
 
 ##### cool dendrogram
 library(ape)
 # vector of colors
-mypal = c("#d73027", "#fc8d59", "#9900ff", "#006666", "#663300", "#4575b4")
+mypal = c('#bd1e24','#e97600','#007256','#7284b7','#964f8e','#0067a7','#172035','#a0333a')
 # cutting dendrogram in 6 clusters
-clus = cutree(hc, 6)
+clus = cutree(hc, 8)
 # plot
 # Size reflects miles per gallon
 pdf(file="circular_dendrograma.pdf",width=20,height=20)
@@ -190,7 +157,6 @@ plot(as.phylo(hc),
       type = "fan", 
       tip.color = mypal[clus], 
       label.offset = 0, 
-      #cex = log(jindex_sif[,3], 10), 
       col = "blue")
 dev.off()
 
