8d89970719d27d124f3b9b71603b362ece85e34a
FRPV <frpvillatoro@gmail.com>
2016-08-23 14:01:39 -0500

    Output path changed

--- scripts/FunSeq2/sort-vcf.sh
+++ scripts/FunSeq2/sort-vcf.sh
@@ -2,7 +2,7 @@
 #BSUB-e sort-vcf.err.%J
 #BSUB-o sort-vcf.out.%J
 
-cd /home/inmegen/r.garcia/src/funseq2-1.2/
+cd /scratch/inmegen/100g/FunSeq2/
 (grep ^"#" ENCODE_Output.vcf; grep -v ^"#" ENCODE_Output.vcf | sort -k1,1 -k2,2n) >ENCODE_Output_sorted.vcf
 bgzip -c ENCODE_Output_sorted.vcf >ENCODE_Output_sorted.vcf.gz
 tabix -p vcf ENCODE_Output_sorted.vcf.gz
