ed1799c063c45da7e8691e4f517d8d912b984d22
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-10-19 00:59:40 -0500

    load all variation, not just snps, use experiment module for config

--- scripts/snp_dendrogram/vcfs2pickles.py
+++ scripts/snp_dendrogram/vcfs2pickles.py
@@ -1,5 +1,6 @@
 import vcf
-import samples
+
+from experiment import samples, paths
 from pprint import pprint
 import pickle
 
@@ -18,29 +19,30 @@ for s in samples.african:
     all[s]=[]    
     
 
-#r = vcf.Reader(open('data/amerindian_chr19.vcf'))
 def sets(path):
+    
     r = vcf.Reader(open(path))
     
     for v in r:
-        if v.is_snp:
-            for s in v.samples:
-                if s.sample in samples.amerindian:
-                    if s.called:
-                        all[s.sample].append((v.CHROM, v.POS, v.REF, str(v.ALT)))
-                elif s.sample in samples.european or s.sample in samples.asian or s.sample in samples.african:
-                    if '1' in s.data.GT:
-                        all[s.sample].append((v.CHROM, v.POS, v.REF, str(v.ALT)))
+        for s in v.samples:
+            if s.sample in samples.amerindian:
+                if s.called:
+                    all[s.sample].append((v.CHROM, v.POS, v.REF, str(v.ALT)))
+            elif s.sample in samples.european \
+                or s.sample in samples.asian \
+                or s.sample in samples.african:
+                if '1' in s.data.GT: # aqui esta la bronca@#solo
+                    all[s.sample].append((v.CHROM, v.POS, v.REF, str(v.ALT)))
 
 
-sets('data/root_1kg_chr19.vcf.gz')
-#sets('data/amerindian_chr19.vcf')
+for path in paths.root:
+    sets(path)
 
-#sets('data/pruebita.vcf')
-#sets('data/pruebota.vcf')
+pprint(all)
 
-for s in all:
-    with open("%s.pickle" % s, 'w') as p:
-        v = set(all[s])
-        pickle.dump(v, p)
+
+# for s in all:
+#     with open("%s.pickle" % s, 'w') as p:
+#         v = set(all[s])
+#         pickle.dump(v, p)
     
